cc4b6988db74f10f240ff79d8e2173a65e182565
Hannington Oduor <oduorhannington13@gmail.com>
2026-08-14 02:27:33 +0300

    reactfrontend

---
+++ src/App.jsx
@@ -0,0 +1,41 @@
+import React from 'react';
+import Navbar from './components/navbar/Navbar';
+import Hero from './components/Hero/hero';
+import Program from './components/Programs/Program';
+import Title from './components/title/title';
+import About from './components/About/About';
+import Campus from './components/Campus/Campus';
+import Testimonials from './components/Testimonials/Testimonials';
+const App = () => {
+  return (
+    <div>
+        <Navbar />
+        <Hero />
+        <div className='container'>
+          <Title 
+          subTitle="Our Programs"
+          Title="What We Offer"
+        />
+        <Program />
+        <About />
+        <Title 
+          subTitle="Gallery"
+          Title="Campus Photos"
+        />
+        <Campus />
+        <Title 
+          subTitle="TESTIMONIALS"
+          Title="What Students Says"
+        />
+        <Testimonials />
+        <Title 
+          subTitle="Contact Us"
+          Title="Get in Touch"
+        />
+
+        </div>
+    </div>
+  );
+}
+
+export default App;
